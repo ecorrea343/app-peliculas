a191cd3129fe83fe03ac2277da3e8ce2b56cd59a
Emmanuel Correa <ebcorreac@gmail.com>
2019-10-14 10:03:49 -0300

    eliminando importacion swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:peliculas/src/widgets/cards_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
