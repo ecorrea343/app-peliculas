d12333243a939008258c084b6a98edd014c8d509
Emmanuel Correa <ebcorreac@gmail.com>
2019-10-15 10:26:04 -0300

    Pasar argumentos de una pantalla a otra -video 112

---
+++ lib/src/pages/pelicula_detallle.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculaDetalle extends StatelessWidget {
+  
+  
+  // PeliculaDetalle(this.pelicula);
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+    final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
+
+
+    return Scaffold(
+
+      body: Center(
+        child: Text(pelicula.title),
+      ),
+      
+    );
+  }
+}
--- lib/src/widgets/movie_horizontal_widget.dart
+++ lib/src/widgets/movie_horizontal_widget.dart
@@ -45,7 +45,7 @@ class MovieHorizontal extends StatelessWidget {
 
 Widget _tarjeta (BuildContext context, Pelicula pelicula){
 
-  return Container(
+  final tarjetaPelicula = Container(
         margin: EdgeInsets.only(right: 10.0),
         child: Column(
           children: <Widget>[
@@ -66,6 +66,12 @@ Widget _tarjeta (BuildContext context, Pelicula pelicula){
             ),
           ],
         ),
+      );
+      return GestureDetector(
+        child: tarjetaPelicula,
+        onTap: (){
+         Navigator.pushNamed(context, 'detalle',arguments: pelicula);
+        },
       );
 }
 
